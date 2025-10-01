cfa6553946d65b4ce4263d0a71c7195bcb230171
klaus-0-0 <jin0667666@gmail.com>
2025-10-01 16:52:57 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-    origin: "https://autopost007.vercel.app",
+    origin: "https://autopostf.onrender.com",
     credentials: true
 }));
 app.use(express.json());
