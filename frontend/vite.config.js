273cf65feea1f5e9676cbf311d1cf2fea7b400b6
klaus-0-0 <jin0667666@gmail.com>
2025-10-01 16:50:44 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(), react()
-  ],
+  plugins: [react()],
+  server: {
+    port: Number(process.env.PORT) || 5173,
+    host: true,
+    allowedHosts: ['whatsapp-32fo.onrender.com'], // keep only one line, remove <<<<<<< HEAD etc
+  },
 })
