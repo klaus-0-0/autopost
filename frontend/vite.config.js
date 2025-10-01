be7895f9d7a618e97b55be297ce881b04df48c2e
klaus-0-0 <jin0667666@gmail.com>
2025-10-01 17:04:30 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   server: {
     port: Number(process.env.PORT) || 5173,
     host: true,
-    allowedHosts: ['https://autopostf.onrender.com'], // keep only one line, remove <<<<<<< HEAD etc
+    allowedHosts: ['autopostf.onrender.com'], // only hostname
   },
 })
