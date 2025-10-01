31e0676951a798eb0fee6405a980a250b8fe3671
klaus-0-0 <jin0667666@gmail.com>
2025-10-01 16:51:19 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   server: {
     port: Number(process.env.PORT) || 5173,
     host: true,
-    allowedHosts: ['whatsapp-32fo.onrender.com'], // keep only one line, remove <<<<<<< HEAD etc
+    allowedHosts: ['https://autopostf.onrender.com'], // keep only one line, remove <<<<<<< HEAD etc
   },
 })
