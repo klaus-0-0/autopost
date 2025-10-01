a83f7c6fade09d730be1e2fda2d7e90cd7b35c57
klaus-0-0 <jin0667666@gmail.com>
2025-10-01 15:04:02 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://autopost007-pkk2.vercel.app/",
     credentials: true
 }));
 app.use(express.json());
