8229b51b20d009c04277af7be3887633a95afddf
klaus-0-0 <jin0667666@gmail.com>
2025-10-01 16:12:30 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,10 @@
-const config = {
-    // apiUrl: "http://localhost:3000/api"
-    apiUrl: "https://autopost007-7icw.vercel.app/api"
+// src/config.js
+const isDev = window.location.hostname === "localhost";
 
-}
+const config = {
+  apiUrl: isDev
+    ? "http://localhost:3000/api"                  // local backend
+    : "https://autopost007-7icw.vercel.app/api"   // deployed backend
+};
+
 export default config;
